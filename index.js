6bf3cbc8ec0412b958601abf3350843f77f9d60a
dmayers96 <dmayers96@yahoo.com>
2022-12-21 15:52:51 +0000

    adding the JS file for country lists

--- index.js
+++ index.js
@@ -45,3 +45,24 @@ exchangeIcon.addEventListener("click", ()=>{
     loadFlag(toCurrency); // calling loadFlag with passing select element (toCurrency) of TO
     getExchangeRate(); // calling getExchangeRate
 })
+
+function getExchangeRate(){
+    const amount = document.querySelector("form input");
+    const exchangeRateTxt = document.querySelector("form .exchange-rate");
+    let amountVal = amount.value;
+    // if user don't enter any value or enter 0 then we'll put 1 value by default in the input field
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    exchangeRateTxt.innerText = "Getting exchange rate...";
+    let url = `https://v6.exchangerate-api.com/v6/0938a671af10f0b9c5fd1060/latest/${fromCurrency.value}`;
+    // fetching api response and returning it with parsing into js obj and in another then method receiving that obj
+    fetch(url).then(response => response.json()).then(result =>{
+        let exchangeRate = result.conversion_rates[toCurrency.value]; // getting user selected TO currency rate
+        let totalExRate = (amountVal * exchangeRate).toFixed(2); // multiplying user entered value with selected TO currency rate
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+    }).catch(() =>{ // if user is offline or any other error occured while fetching data then catch function will run
+        exchangeRateTxt.innerText = "Something went wrong";
+    });
+}
