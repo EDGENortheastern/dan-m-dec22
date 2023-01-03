9dedd9f47771e956deb718969ceb2d711e02c971
dmayers96 <dmayers96@yahoo.com>
2023-01-03 14:47:28 +0000

    last commit

--- index.test.js
+++ index.test.js
@@ -20,7 +20,7 @@ test("That function convertCurrency is accurate",()=>{
     expect(convertCurrency("2", "2")).toBe("4.00");
     expect(convertCurrency("1000", "1.30")).toBe("1300.00");
 });
-test('Thar function convertCurrency works with many decimals', () => {
+test('That function convertCurrency works with many decimals', () => {
     expect(parseFloat(convertCurrency("1000.567", "0.57"))).toBeCloseTo(parseFloat("570.32"));
     expect(parseFloat(convertCurrency("547.405832", "0.11"))).toBeCloseTo(parseFloat("60.21"));
 });
